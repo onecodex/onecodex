3c8c10be901b849df00f6c2ed5367b31ff5656f7
Nick Greenfield <boyd.greenfield@gmail.com>
2014-10-30 11:04:37 -0700

    Bump to v0.0.6 for PyPI release.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='onecodex',
-    version='0.0.5',
+    version='0.0.6',
     url='https://www.onecodex.com/',
     license='MIT',
     author='Reference Genomics, Inc.',
