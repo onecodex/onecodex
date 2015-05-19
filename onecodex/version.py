a41c44b9ba4914355f1222ac3ff513c6894bf354
Nick Greenfield <boyd.greenfield@gmail.com>
2015-05-19 16:48:03 -0700

    Update to v0.0.8

--- onecodex/version.py
+++ onecodex/version.py
@@ -1,3 +1,3 @@
-VERSION = "0.0.7"
+VERSION = "0.0.8"
 API_VERSION = "v0"
 API_LINK = "http://docs.onecodex.com/v0/docs"
--- tests/test_version.py
+++ tests/test_version.py
@@ -2,5 +2,5 @@ from onecodex import version
 
 
 def test_correct_version_exposed():
-    assert version.VERSION == "0.0.7"
+    assert version.VERSION == "0.0.8"
     assert version.API_VERSION == "v0"
