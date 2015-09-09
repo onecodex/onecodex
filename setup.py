745ef5fad11783d89e9a625b72e766379f10026d
Nick Greenfield <boyd.greenfield@gmail.com>
2015-09-09 15:52:07 -0700

    Bump to v0.1.1.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='onecodex',
-    version='0.1.0',
+    version='0.1.1',
     url='https://www.onecodex.com/',
     license='MIT',
     author='Reference Genomics, Inc.',
