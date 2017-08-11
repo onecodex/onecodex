88a5b25ed5b51fc4ab46d4676cdf7112dcf620ad
Cindy <czwang@bu.edu>
2017-08-11 15:58:23 -0700

    plot heatmap

--- onecodex/viz/heatmap.py
+++ onecodex/viz/heatmap.py
@@ -1,33 +1,33 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn
 
+from onecodex.exceptions import OneCodexException
 from onecodex.viz.helpers import collate_analysis_results, normalize_analyses
 
 
-def plot_heatmap(analyses, title=None, min_abund=0.01, metric='abundance'):
-    # min_abund: Only plot taxa that reach this abundance in at least one sample
+def plot_heatmap(analyses, title=None, threshold=None, top_n=20, metric='abundance'):
     # metric: 'abundance' or 'readcount'
+    assert len(analyses) > 1
+
+    if not (threshold or top_n):
+        raise OneCodexException('Please set either "threshold" or "top_n"')
 
     a, metadata = normalize_analyses(analyses)
     df = collate_analysis_results(a, metric=metric)
 
-    # Only keep the metadata for samples that have microbial abundance data
-    # metadata = metadata.loc[[ix for ix in df.index.values if ix in metadata.index.values], :]
-    # metadata = [ix for ix in df.index.values if ix in metadata.index.values]
-
-    # Order the abundance data the same as the metadata
-    # df = df.loc[metadata.index.values, :]
-
-    # Name the taxa
-    df.rename(columns=lambda t: "{} ({})".format(df['tax_name'], t), inplace=True)
-
-    # Name the samples (show the name if specified, otherwise fall back to the filename)
-    df['display_name'] = [r['name'] if r['name'] is not None else r['filename']
-                          for ix, r in metadata.iterrows()]
+    df.columns = ['{} ({})'.format(v[1], v[0]) for v in df.columns.values]
+    df['display_name'] = [m.name if m.name is not None else m.sample.filename for m in metadata]
     df.set_index('display_name', inplace=True)
 
-    # Plot a heatmap of the raw abundances
-    g = seaborn.clustermap(df.loc[:, df.max() >= min_abund])
+    if threshold and top_n:
+        idx = df.sum(axis=0).sort_values(ascending=False).head(top_n).index
+        df = df.loc[:, idx]  # can't use idx with multiple conditions
+        g = seaborn.clustermap(df.loc[:, df.max() >= threshold])
+    elif threshold:
+        g = seaborn.clustermap(df.loc[:, df.max() >= threshold])
+    elif top_n:
+        idx = df.sum(axis=0).sort_values(ascending=False).head(top_n).index
+        g = seaborn.clustermap(df.loc[:, idx])
 
     # Rotate the margin labels
     plt.setp(g.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
