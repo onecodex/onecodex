c4f443583cae9884b9ac84125b5e1cbd6b54e505
Cindy <czwang@bu.edu>
2017-08-15 14:21:06 -0700

    fixup: normalize data in beta counts

--- onecodex/lib/distance_metrics.py
+++ onecodex/lib/distance_metrics.py
@@ -1,7 +1,7 @@
 import skbio.diversity
 
 from onecodex.lib.taxonomy import generate_skbio_tree, prune_to_rank
-from onecodex.viz.helpers import collate_analysis_results
+from onecodex.viz.helpers import normalize_analyses, collate_analysis_results
 
 
 ACCEPTABLE_FIELDS = ['abundance', 'readcount_w_children', 'readcount']
@@ -17,7 +17,8 @@ def alpha_counts(classification, field='readcount_w_children', rank='species'):
 
 
 def beta_counts(classifications, field='readcount_w_children', rank='species'):
-    df = collate_analysis_results(classifications, field=field)
+    normed_analyses, _ = normalize_analyses(classifications)
+    df = collate_analysis_results(normed_analyses, field=field)
     df = df.loc[:, [i[2] == rank for i in df.columns]]
 
     tax_ids = [t[0] for t in df.columns.values]
