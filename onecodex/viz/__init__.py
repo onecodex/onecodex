db73b5fd32e8d90e60a825dbd7bcfdac35bc8c49
Nick Greenfield <boyd.greenfield@gmail.com>
2017-08-19 18:33:13 -0700

    Update namespacing for viz and distance modules, defaults exposed on Api
    * Implement ModuleAlias class to hold only objects/functions in __all__
    * Hide viz implementation files from module namespace
    * Add __all__ to distance module, do not setattr on Api instance

--- onecodex/viz/__init__.py
+++ onecodex/viz/__init__.py
@@ -1,6 +1,6 @@
-from onecodex.viz.heatmap import plot_heatmap
-from onecodex.viz.pca import plot_pca
-from onecodex.viz.distance import plot_distance
-from onecodex.viz.metadata import plot_metadata
+from ._heatmap import plot_heatmap
+from ._pca import plot_pca
+from ._distance import plot_distance
+from ._metadata import plot_metadata
 
 __all__ = ['plot_heatmap', 'plot_pca', 'plot_distance', 'plot_metadata']
