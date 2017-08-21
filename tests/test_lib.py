e2acdd85303344a163ddb5340d33ecc1af593841
Nick Greenfield <boyd.greenfield@gmail.com>
2017-08-21 11:26:55 -0700

    Release v0.2.4 with add'l plotting and skbio integration support
    * Also removes login test that hits our live server with bad credentials
      (could lead to blocking IPs of eager client library testers)

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,7 +1,7 @@
 """
 Unit tests for One Codex
 """
-from onecodex.lib.auth import check_version, fetch_api_key_from_uname
+from onecodex.lib.auth import check_version
 from onecodex.version import __version__
 
 
@@ -14,10 +14,3 @@ def test_check_version():
 
     assert not should_upgrade
     assert msg is None or msg.startswith('Please upgrade your client to the latest version')
-
-
-def test_login():
-    # TODO: Remove Internet dependency here -- need a version response mock
-    #       May want a live tests suite that *does* do this though for awareness? Investigate.
-    resp = fetch_api_key_from_uname('test', 'test', SERVER)
-    assert resp is None  # should not be able to log in with this
