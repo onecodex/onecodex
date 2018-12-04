cfc5a33774f2fa85ad8994004ed8c1822c0a7e88
polyatail <andrewscz@gmail.com>
2018-12-04 15:33:17 -0800

    Add interactive heatmap visualization (#132) * Plots top N taxa or taxa above specified threshold * Relabel given tax IDs with taxonomic names

--- onecodex/viz/_heatmap.py
+++ onecodex/viz/_heatmap.py
@@ -1,53 +1,106 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pandas as pd
+import altair as alt
 
 from onecodex.exceptions import OneCodexException
 from onecodex.helpers import collate_classification_results, normalize_classifications
 
 
 def plot_heatmap(analyses, top_n=20, threshold=None,
-                 title=None, label=None, xlabel=None, ylabel=None,
-                 field='readcount_w_children', rank=None, normalize=False, **kwargs):
-    """Plot a heatmap of classification results by sample.
+                 title=None, xlabel=None, ylabel=None, tooltip=None,
+                 field='readcount_w_children', rank=None, normalize=True):
+    """Plot heatmap of taxa abundance/count data for several samples.
 
-    Additional **kwargs are passed to Seaborn's `sns.clustermap`.
+    analyses (list) -- list of Samples, Classifications, or Analyses objects to be plotted
+    top_n (int) -- display the top N most abundant taxa
+    threshold (float) -- display only taxa more abundant than this threshold
+
+    field ('readcount_w_children' | 'readcount' | 'abundance')
+        - 'readcount_w_children': total reads of this taxon and all its descendants
+        - 'readcount': total reads of this taxon
+        - 'abundance': genome size-normalized relative abundances, from shotgun sequencing
+    rank (None | 'kingdom' | 'phylum' | 'class' | 'order' | 'family' | 'genus' | 'species')
+        - None: include all ranks
+        - 'kingdom' or others: restrict analysis to taxa at this rank
+    normalize (bool): convert from read counts to relative abundances (each sample sums to 1.0)
+
+    title (string) -- main title of the plot
+    xlabel, ylabel (string) -- axes labels
+    tooltip (list) -- display these metadata fields when points are hovered over
     """
+
     if not (threshold or top_n):
         raise OneCodexException('Please set either "threshold" or "top_n"')
 
-    normed_classifications, metadata = normalize_classifications(analyses, label=label)
-    df, tax_info = collate_classification_results(normed_classifications, field=field, rank=rank,
-                                                  normalize=normalize)
+    normed_classifications, metadata = normalize_classifications(analyses)
+    df, tax_info = collate_classification_results(normed_classifications, field=field,
+                                                  rank=rank, normalize=normalize)
+
+    metadata.index = df.index
 
     if len(df) < 2:
         raise OneCodexException('`plot_heatmap` requires 2 or more valid classification results.')
 
     df.columns = ['{} ({})'.format(tax_info[tax_id]['name'], tax_id) for tax_id in df.columns.values]
-    df.index = metadata.loc[:, '_display_name']
-    df.index.name = ''
 
-    sns.set(style=kwargs.pop('style', 'darkgrid'))
-
-    if threshold and top_n:
+    # filter out taxa to plot
+    if top_n:
         idx = df.sum(axis=0).sort_values(ascending=False).head(top_n).index
-        df = df.loc[:, idx]  # can't use idx with multiple conditions
-        g = sns.clustermap(df.loc[:, df.max() >= threshold], **kwargs)
-    elif threshold:
-        g = sns.clustermap(df.loc[:, df.max() >= threshold], **kwargs)
-    elif top_n:
-        idx = df.sum(axis=0).sort_values(ascending=False).head(top_n).index
-        g = sns.clustermap(df.loc[:, idx], **kwargs)
+        df = df.loc[:, idx]
 
-    # Rotate the margin labels
-    plt.setp(g.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
+    if threshold:
+        df = df.loc[:, df.max() >= threshold]
 
-    # Labels
-    if xlabel is not None:
-        plt.gca().set_xlabel(xlabel)
-    if ylabel is not None:
-        plt.gca().set_ylabel(ylabel)
+    # prepare tooltips
+    if tooltip:
+        if not isinstance(tooltip, list):
+            tooltip = [tooltip]
+    else:
+        tooltip = []
+
+    for param in tooltip:
+        if param not in metadata and param != df.index.name:
+            raise OneCodexException('Column {} not found in metadata'.format(param))
+
+    # transfer data into something altair can handle
+    plot_data = {
+        'classification_id': [],
+        'display_name': [],
+        'taxon': [],
+        'value': []
+    }
+
+    for taxon in df:
+        for sample, abundance in df[taxon].iteritems():
+            plot_data['classification_id'].append(sample)
+            plot_data['display_name'].append(metadata['_display_name'][sample])
+            plot_data['taxon'].append(taxon)
+            plot_data['value'].append(abundance)
+
+            for tip in tooltip:
+                try:
+                    plot_data[tip].append(metadata[tip][sample])
+                except KeyError:
+                    plot_data[tip] = [metadata[tip][sample]]
+
+    plot_data = pd.DataFrame(data=plot_data)
+
+    alt_kwargs = dict(
+        x=alt.X('display_name:N', axis=alt.Axis(title=xlabel)),
+        y=alt.Y('taxon:N', axis=alt.Axis(title=ylabel)),
+        color='value:Q',
+        tooltip=['value:Q'] + tooltip,
+        href='url:N',
+        url='https://app.onecodex.com/classification/' + alt.datum.classification_id
+    )
+
+    alt.renderers.enable('notebook')
+
+    chart = alt.Chart(plot_data) \
+               .transform_calculate(url=alt_kwargs.pop('url')) \
+               .mark_rect() \
+               .encode(**alt_kwargs)
 
     if title:
-        g.fig.suptitle(title)
+        chart = chart.properties(title=title)
 
-    plt.show()
+    chart.interactive().display()
