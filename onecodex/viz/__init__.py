dde5a2f9dc0df8277faaad0bc48cc6a6eb73734f
polyatail <andrewscz@gmail.com>
2019-01-01 11:58:59 -0800

    Choose altair renderer more intelligently. Closes #145. * If inside notebook service nbconvert, render high-res PNGs * If inside ipython, use the notebook renderer

--- onecodex/viz/__init__.py
+++ onecodex/viz/__init__.py
@@ -1,4 +1,5 @@
 import altair as alt
+import os
 
 from onecodex.viz._heatmap import VizHeatmapMixin
 from onecodex.viz._pca import VizPCAMixin
@@ -48,4 +49,21 @@ def onecodex_theme():
 
 alt.themes.register('onecodex', onecodex_theme)
 alt.themes.enable('onecodex')
-alt.renderers.enable('notebook')
+
+def onecodex_renderer(spec, **metadata):
+    metadata['scale_factor'] = 1.5
+    return alt.vegalite.v2.display.png_renderer(spec, **metadata)
+
+alt.renderers.register('onecodex', onecodex_renderer)
+
+if os.getenv('OCX_NBCONVERT', False):
+    # if run from notebook service nbconvert, render as a high-res PNG
+    alt.renderers.enable('onecodex')
+else:
+    try:
+        # if inside ipython, enable the notebook renderer
+        get_ipython()
+        alt.renderers.enable('notebook')
+    except NameError:
+        # otherwise, use altair's default renderer
+        alt.renderers.enable('default')
