Add heatmap facets based on metadata. Closes #151. * Track df normalization via flag in ClassificationsDataFrame * New methods _cluster_by_sample and _cluster_by_taxa * Fix bug where dendrogram didn't line up with heatmap

onecodex/helpers.py
@@ -36,14 +36,15 @@ class ClassificationsDataFrame(pd.DataFrame):
             referenced in this analysis.
     """
 
-    _metadata = ['ocx_rank', 'ocx_field', 'ocx_taxonomy', 'ocx_metadata']
+    _metadata = ['ocx_rank', 'ocx_field', 'ocx_taxonomy', 'ocx_metadata', 'ocx_normalized']
 
     def __init__(self, data=None, index=None, columns=None, dtype=None, copy=False, ocx_rank=None,
-                 ocx_field=None, ocx_taxonomy=None, ocx_metadata=None):
+                 ocx_field=None, ocx_taxonomy=None, ocx_metadata=None, ocx_normalized=None):
         self.ocx_rank = ocx_rank
         self.ocx_field = ocx_field
         self.ocx_taxonomy = ocx_taxonomy
         self.ocx_metadata = ocx_metadata
+        self.ocx_normalized = ocx_normalized
 
         pd.DataFrame.__init__(self, data=data, index=index, columns=columns, dtype=dtype, copy=copy)
 
@@ -52,14 +53,16 @@ class ClassificationsDataFrame(pd.DataFrame):
         # we explicitly do *not* pass rank on to manipulated ClassificationsDataFrame. we don't know
         # how the data has been manipulated, and it may no longer be accurate
         return partial(ClassificationsDataFrame, ocx_rank=None, ocx_field=self.ocx_field,
-                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata)
+                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata,
+                       ocx_normalized=self.ocx_normalized)
 
     @property
     def _constructor_sliced(self):
         # we explicitly do *not* pass rank on to manipulated ClassificationsDataFrame. we don't know
         # how the data has been manipulated, and it may no longer be accurate
         return partial(ClassificationsSeries, ocx_rank=None, ocx_field=self.ocx_field,
-                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata)
+                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata,
+                       ocx_normalized=self.ocx_normalized)
 
 
 class ClassificationsSeries(pd.Series):
@@ -68,14 +71,16 @@ class ClassificationsSeries(pd.Series):
     """
 
     # 'name' is a piece of metadata specified by pd.Series--it's not ours
-    _metadata = ['name', 'ocx_rank', 'ocx_field', 'ocx_taxonomy', 'ocx_metadata']
+    _metadata = ['name', 'ocx_rank', 'ocx_field', 'ocx_taxonomy', 'ocx_metadata', 'ocx_normalized']
 
     def __init__(self, data=None, index=None, dtype=None, name=None, copy=False, fastpath=False,
-                 ocx_rank=None, ocx_field=None, ocx_taxonomy=None, ocx_metadata=None):
+                 ocx_rank=None, ocx_field=None, ocx_taxonomy=None, ocx_metadata=None,
+                 ocx_normalized=None):
         self.ocx_rank = ocx_rank
         self.ocx_field = ocx_field
         self.ocx_taxonomy = ocx_taxonomy
         self.ocx_metadata = ocx_metadata
+        self.ocx_normalized = ocx_normalized
 
         pd.Series.__init__(self, data=data, index=index, dtype=dtype, name=name, copy=copy, fastpath=fastpath)
 
@@ -84,14 +89,16 @@ class ClassificationsSeries(pd.Series):
         # we explicitly do *not* pass rank on to manipulated ClassificationsDataFrames. we don't know
         # how the data has been manipulated, and it may no longer be accurate
         return partial(ClassificationsSeries, ocx_rank=None, ocx_field=self.ocx_field,
-                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata)
+                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata,
+                       ocx_normalized=self.ocx_normalized)
 
     @property
     def _constructor_expanddim(self):
         # we explicitly do *not* pass rank on to manipulated ClassificationsDataFrame. we don't know
         # how the data has been manipulated, and it may no longer be accurate
         return partial(ClassificationsDataFrame, ocx_rank=None, ocx_field=self.ocx_field,
-                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata)
+                       ocx_taxonomy=self.ocx_taxonomy, ocx_metadata=self.ocx_metadata,
+                       ocx_normalized=self.ocx_normalized)
 
 
 class AnalysisMixin(VizPCAMixin, VizHeatmapMixin, VizMetadataMixin, VizDistanceMixin):
@@ -128,7 +135,9 @@ class AnalysisMixin(VizPCAMixin, VizHeatmapMixin, VizMetadataMixin, VizDistanceM
         It's possible that the _results df has already been normalized, which can cause some
         methods to fail. This method lets us guess whether that's true and act accordingly.
         """
-        return bool((self._results.sum(axis=1).round(4) == 1.0).all())
+        return getattr(self, '_normalized', False) or \
+               getattr(self, '_field', None) == 'abundance' or \
+               bool((self._results.sum(axis=1).round(4) == 1.0).all())  # noqa
 
     def _metadata_fetch(self, metadata_fields):
         """Takes a list of metadata fields, some of which can contain taxon names or taxon IDs, and
@@ -313,6 +322,7 @@ class AnalysisMixin(VizPCAMixin, VizHeatmapMixin, VizMetadataMixin, VizDistanceM
             'ocx_rank': rank,
             'ocx_field': self._field,
             'ocx_taxonomy': self.taxonomy.copy(),
+            'ocx_normalized': normalize
         }
 
         # generate long-format table
@@ -356,6 +366,7 @@ class OneCodexAccessor(AnalysisMixin):
         # copy data from the ClassificationsDataFrame to a new instance of AnalysisMethods
         self.metadata = pandas_obj.ocx_metadata
         self.taxonomy = pandas_obj.ocx_taxonomy
+        self._normalized = pandas_obj.ocx_normalized
         self._field = pandas_obj.ocx_field
         self._rank = pandas_obj.ocx_rank
         self._results = pandas_obj

onecodex/viz/_distance.py
@@ -21,9 +21,6 @@ class VizDistanceMixin(DistanceMixin):
         if rank is None:
             raise OneCodexException('Please specify a rank or \'auto\' to choose automatically')
 
-        if len(self._results) < 2:
-            raise OneCodexException('`plot_distance` requires 2 or more valid classification results.')
-
         # if taxonomy trees are inconsistent, unifrac will not work
         if callable(metric):
             distances = metric(self, rank=rank)
@@ -42,6 +39,39 @@ class VizDistanceMixin(DistanceMixin):
                                     'weighted_unifrac, unweighted_unifrac')
 
         return distances
+
+    def _cluster_by_sample(self, rank='auto', metric='braycurtis', linkage='average'):
+        if metric == 'euclidean':
+            dist_matrix = euclidean_distances(self._results).round(6)
+        else:
+            dist_matrix = self._compute_distance(rank=rank, metric=metric).to_data_frame().round(6)
+        clustering = hierarchy.linkage(squareform(dist_matrix), method=linkage)
+        scipy_tree = hierarchy.dendrogram(clustering, no_plot=True)
+        ids_in_order = [self._results.index[int(x)] for x in scipy_tree['ivl']]
+        labels_in_order = [self.metadata['_display_name'][t] for t in ids_in_order]
+
+        return {
+            'dist_matrix': dist_matrix,
+            'clustering': clustering,
+            'scipy_tree': scipy_tree,
+            'ids_in_order': ids_in_order,
+            'labels_in_order': labels_in_order
+        }
+
+    def _cluster_by_taxa(self, linkage='average'):
+        dist_matrix = euclidean_distances(self._results.T).round(6)
+        clustering = hierarchy.linkage(squareform(dist_matrix), method=linkage)
+        scipy_tree = hierarchy.dendrogram(clustering, no_plot=True)
+        ids_in_order = [self._results.T.index[int(x)] for x in scipy_tree['ivl']]
+        labels_in_order = ['{} ({})'.format(self.taxonomy['name'][t], t) for t in ids_in_order]
+
+        return {
+            'dist_matrix': dist_matrix,
+            'clustering': clustering,
+            'scipy_tree': scipy_tree,
+            'ids_in_order': ids_in_order,
+            'labels_in_order': labels_in_order
+        }
 
     def plot_distance(self, rank='auto', metric='braycurtis',
                       title=None, xlabel=None, ylabel=None, tooltip=None, return_chart=False,
@@ -73,7 +103,8 @@ class VizDistanceMixin(DistanceMixin):
 
         >>> plot_distance(rank='genus', metric='unifrac')
         """
-        distances = self._compute_distance(rank, metric)
+        if len(self._results) < 2:
+            raise OneCodexException('`plot_distance` requires 2 or more valid classification results.')
 
         # this will be passed to the heatmap chart as a dataframe eventually
         plot_data = {
@@ -103,13 +134,15 @@ class VizDistanceMixin(DistanceMixin):
 
             formatted_fields.append(field_group)
 
+        clust = self._cluster_by_sample(rank=rank, metric=metric, linkage=linkage)
+
         # must convert to long format for heatmap plotting
-        for idx1, id1 in enumerate(distances.ids):
-            for idx2, id2 in enumerate(distances.ids):
+        for idx1, id1 in enumerate(clust['dist_matrix'].index):
+            for idx2, id2 in enumerate(clust['dist_matrix'].index):
                 if idx1 == idx2:
                     plot_data['Distance'].append(np.nan)
                 else:
-                    plot_data['Distance'].append(distances.data[idx1][idx2])
+                    plot_data['Distance'].append(clust['dist_matrix'].iloc[idx1, idx2])
 
                 plot_data['1) Label'].append(self.metadata['_display_name'][id1])
                 plot_data['2) Label'].append(self.metadata['_display_name'][id2])
@@ -121,20 +154,11 @@ class VizDistanceMixin(DistanceMixin):
 
         plot_data = pd.DataFrame(data=plot_data)
 
-        # turn the distances returned by skbio into a simple distance matrix
-        dists = distances.to_data_frame()
-
-        # here we use scipy to perform average-linkage clustering on the distance matrix
-        clustering = hierarchy.linkage(squareform(dists), method=linkage)
-        tree = hierarchy.dendrogram(clustering, no_plot=True)
-        class_ids_in_order = [dists.index[int(x)] for x in tree['ivl']]
-        names_in_order = self.metadata['_display_name'][class_ids_in_order].tolist()
-
         # it's important to tell altair to order the cells in the heatmap according to the clustering
         # obtained from scipy
         alt_kwargs = dict(
-            x=alt.X('1) Label:N', axis=alt.Axis(title=xlabel), sort=names_in_order),
-            y=alt.Y('2) Label:N', axis=alt.Axis(title=ylabel, orient='right'), sort=names_in_order),
+            x=alt.X('1) Label:N', axis=alt.Axis(title=xlabel), sort=clust['labels_in_order']),
+            y=alt.Y('2) Label:N', axis=alt.Axis(title=ylabel, orient='right'), sort=clust['labels_in_order']),
             color='Distance:Q',
             tooltip=['1) Label', '2) Label', 'Distance:Q'] + list(chain.from_iterable(formatted_fields)),
             href='url:N',
@@ -142,8 +166,8 @@ class VizDistanceMixin(DistanceMixin):
         )
 
         chart = alt.Chart(plot_data,
-                          width=15 * len(distances.ids),
-                          height=15 * len(distances.ids)) \
+                          width=15 * len(clust['dist_matrix'].index),
+                          height=15 * len(clust['dist_matrix'].index)) \
                    .transform_calculate(url=alt_kwargs.pop('url')) \
                    .mark_rect() \
                    .encode(**alt_kwargs)
@@ -151,7 +175,7 @@ class VizDistanceMixin(DistanceMixin):
         if title:
             chart = chart.properties(title=title)
 
-        dendro_chart = dendrogram(tree)
+        dendro_chart = dendrogram(clust['scipy_tree'])
 
         if return_chart:
             return (dendro_chart | chart)
@@ -206,6 +230,9 @@ class VizDistanceMixin(DistanceMixin):
         they truly represent the calculated distances. They do not reflect how well the distance
         metric captures similarities between the underlying data (in this case, an OTU table).
         """
+        if len(self._results) < 2:
+            raise OneCodexException('`plot_mds` requires 2 or more valid classification results.')
+
         dists = self._compute_distance(rank, metric).to_data_frame()
 
         # here we figure out what to put in the tooltips and get the appropriate data