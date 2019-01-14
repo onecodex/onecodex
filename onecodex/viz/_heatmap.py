Add heatmap facets based on metadata. Closes #151. * Track df normalization via flag in ClassificationsDataFrame * New methods _cluster_by_sample and _cluster_by_taxa * Fix bug where dendrogram didn't line up with heatmap

onecodex/viz/_heatmap.py
@@ -1,15 +1,13 @@
 import altair as alt
-from scipy.cluster import hierarchy
-from scipy.spatial.distance import squareform
-from sklearn.metrics.pairwise import euclidean_distances
+import pandas as pd
 
 from onecodex.exceptions import OneCodexException
 
 
 class VizHeatmapMixin(object):
-    def plot_heatmap(self, rank='auto', normalize='auto', top_n=20, threshold=None,
+    def plot_heatmap(self, rank='auto', normalize='auto', top_n='auto', threshold='auto',
                      title=None, xlabel=None, ylabel=None, tooltip=None, return_chart=False,
-                     linkage='average'):
+                     linkage='average', haxis=None, metric='euclidean'):
         """Plot heatmap of taxa abundance/count data for several samples.
 
         Parameters
@@ -22,6 +20,11 @@ class VizHeatmapMixin(object):
         return_chart : `bool`, optional
             When True, return an `altair.Chart` object instead of displaying the resulting plot in
             the current notebook.
+        haxis : `string`, optional
+            The metadata field (or tuple containing multiple categorical fields) used to group
+            samples together. Each group of samples will be clustered independently.
+        metric : {'braycurtis', 'manhattan', 'jaccard', 'unifrac', 'unweighted_unifrac}, optional
+            Function to use when calculating the distance between two samples.
         linkage : {'average', 'single', 'complete', 'weighted', 'centroid', 'median'}
             The type of linkage to use when clustering axes.
         top_n : `int`, optional
@@ -54,6 +57,14 @@ class VizHeatmapMixin(object):
         if len(self._results) < 2:
             raise OneCodexException('`plot_heatmap` requires 2 or more valid classification results.')
 
+        if top_n == 'auto' and threshold == 'auto':
+            top_n = 10
+            threshold = None
+        elif top_n == 'auto' and threshold != 'auto':
+            top_n = None
+        elif top_n != 'auto' and threshold == 'auto':
+            threshold = None
+
         df = self.to_df(
             rank=rank,
             normalize=normalize,
@@ -68,6 +79,9 @@ class VizHeatmapMixin(object):
         else:
             tooltip = []
 
+        if haxis:
+            tooltip.append(haxis)
+
         magic_metadata, magic_fields = self._metadata_fetch(tooltip)
 
         # add columns for prettier display
@@ -78,47 +92,167 @@ class VizHeatmapMixin(object):
         for f in tooltip:
             df[magic_fields[f]] = magic_metadata[magic_fields[f]][df['classification_id']].tolist()
 
-        # use scipy to perform average-linkage clustering on euclidean distances (by taxa)
-        df_for_clustering = self.to_df(
-            rank=rank,
-            normalize=normalize,
-            top_n=top_n,
-            threshold=threshold
-        ).T
-        taxa_dist = euclidean_distances(df_for_clustering).round(6)
-        clustering = hierarchy.linkage(squareform(taxa_dist), method=linkage)
-        tree = hierarchy.dendrogram(clustering, no_plot=True)
-        tax_ids_in_order = [df_for_clustering.index[int(x)] for x in tree['ivl']]
-        tax_names_in_order = ['{} ({})'.format(self.taxonomy['name'][t], t) for t in tax_ids_in_order]
+        # if we've already been normalized, we must cluster samples by euclidean distance. beta
+        # diversity measures won't work with normalized distances.
+        if self._guess_normalized():
+            if metric != 'euclidean':
+                raise OneCodexException('Results are normalized. Please re-run with metric=euclidean')
 
-        # use scipy to perform average-linkage clustering on euclidean distances (by sample)
-        df_for_clustering = df_for_clustering.T
-        sample_dist = euclidean_distances(df_for_clustering).round(6)
-        clustering = hierarchy.linkage(squareform(sample_dist), method=linkage)
-        tree = hierarchy.dendrogram(clustering, no_plot=True)
-        c_ids_in_order = [df_for_clustering.index[int(x)] for x in tree['ivl']]
-        labels_in_order = [self.metadata['_display_name'][t] for t in c_ids_in_order]
+            df_sample_cluster = self.to_df(
+                rank=rank, normalize=normalize, top_n=top_n, threshold=threshold
+            )
+            df_taxa_cluster = df_sample_cluster
+        else:
+            df_sample_cluster = self.to_df(
+                rank=rank, normalize=False, top_n=top_n, threshold=threshold
+            )
+
+            df_taxa_cluster = self.to_df(
+                rank=rank, normalize=normalize, top_n=top_n, threshold=threshold
+            )
+
+        if haxis is None:
+            # cluster only once
+            sample_cluster = df_sample_cluster.ocx._cluster_by_sample(rank=rank, metric=metric, linkage=linkage)
+            taxa_cluster = df_taxa_cluster.ocx._cluster_by_taxa(linkage=linkage)
+
+            labels_in_order = sample_cluster['labels_in_order']
+        else:
+            if not (pd.api.types.is_bool_dtype(df[magic_fields[haxis]]) or  # noqa
+                    pd.api.types.is_categorical_dtype(df[magic_fields[haxis]]) or  # noqa
+                    pd.api.types.is_object_dtype(df[magic_fields[haxis]])):  # noqa
+                raise OneCodexException('Metadata field on horizontal axis can not be numerical')
+
+            # taxa clustered only once
+            taxa_cluster = df_taxa_cluster.ocx._cluster_by_taxa(linkage=linkage)
+
+            # cluster samples for every group of metadata
+            groups = magic_metadata[magic_fields[haxis]].unique()
+            cluster_by_group = {}
+
+            labels_in_order = []
+
+            plot_data = {'x': [], 'y': [], 'o': [], 'b': []}
+            label_data = {'x': [], 'y': [], 'label': []}
+
+            for idx, group in enumerate(groups):
+                # if value of metadata field is 'null', we have to use pd.isnull, can't use 'is None'
+                if pd.isnull(group):
+                    c_ids_in_group = magic_metadata.index[pd.isnull(magic_metadata[magic_fields[haxis]])]
+                else:
+                    c_ids_in_group = magic_metadata.index[magic_metadata[magic_fields[haxis]] == group]
+
+                if len(c_ids_in_group) == 0:
+                    continue
+
+                sample_slice = df_sample_cluster.loc[c_ids_in_group]
+
+                if len(c_ids_in_group) < 3:
+                    # clustering not possible in this case
+                    cluster_by_group[group] = {
+                        'labels_in_order': self.metadata._display_name[c_ids_in_group]
+                    }
+                else:
+                    cluster_by_group[group] = sample_slice.ocx._cluster_by_sample(
+                        rank=rank, metric=metric, linkage=linkage
+                    )
+
+                plot_data['x'].append(len(labels_in_order) + 0.25)
+
+                labels_in_order.extend(cluster_by_group[group]['labels_in_order'])
+
+                plot_data['x'].append(len(labels_in_order) - 0.25)
+                plot_data['y'].extend([0, 0])
+                plot_data['o'].extend([0, 1])
+                plot_data['b'].extend([idx, idx])
+
+                label_data['x'].append(sum(plot_data['x'][-2:]) / 2)
+                label_data['y'].append(1)
+                label_data['label'].append(str(group))
+
+            label_bars = alt.Chart(
+                pd.DataFrame(plot_data), width=15 * len(df_sample_cluster.index), height=10
+            ).mark_line(
+                point=False, opacity=0.5
+            ).encode(
+                x=alt.X(
+                    'x',
+                    axis=None,
+                    scale=alt.Scale(
+                        domain=[0, len(df_sample_cluster.index)],
+                        zero=True,
+                        nice=False
+                    )
+                ),
+                y=alt.Y(
+                    'y',
+                    axis=None
+                ),
+                order='o',
+                color=alt.Color(
+                    'b:N',
+                    scale=alt.Scale(
+                        domain=list(range(idx + 1)),
+                        range=['black'] * (idx + 1)
+                    ),
+                    legend=None
+                )
+            )
+
+            label_text = alt.Chart(
+                pd.DataFrame(label_data), width=15 * len(df_sample_cluster.index), height=10
+            ).mark_text(
+                align='center', baseline='middle'
+            ).encode(
+                x=alt.X(
+                    'x',
+                    axis=None,
+                    scale=alt.Scale(
+                        domain=[0, len(df_sample_cluster.index)],
+                        zero=True,
+                        nice=False
+                    )
+                ),
+                y=alt.Y(
+                    'y',
+                    axis=alt.Axis(
+                        title=haxis, ticks=False, domain=False, labels=False
+                    )),
+                text='label'
+            )
+
+            top_label = alt.layer(label_text, label_bars)
 
         alt_kwargs = dict(
             x=alt.X('display_name:N', axis=alt.Axis(title=xlabel), sort=labels_in_order),
-            y=alt.Y('tax_name:N', axis=alt.Axis(title=ylabel), sort=tax_names_in_order),
+            y=alt.Y('tax_name:N', axis=alt.Axis(title=ylabel), sort=taxa_cluster['labels_in_order']),
             color='{}:Q'.format(self._field),
-            tooltip=['{}:Q'.format(self._field)] + tooltip,
+            tooltip=['{}:Q'.format(self._field)] + [magic_fields[f] for f in tooltip],
             href='url:N',
             url='https://app.onecodex.com/classification/' + alt.datum.classification_id
         )
 
-        chart = alt.Chart(df,
-                          width=15 * len(df['classification_id'].unique()),
-                          height=15 * len(df['tax_id'].unique())) \
-                   .transform_calculate(url=alt_kwargs.pop('url')) \
-                   .mark_rect() \
-                   .encode(**alt_kwargs)
+        chart = alt.Chart(
+            df,
+            width=15 * len(df['classification_id'].unique()),
+            height=15 * len(df['tax_id'].unique())
+        ).transform_calculate(
+            url=alt_kwargs.pop('url')
+        ).mark_rect(
+        ).encode(
+            **alt_kwargs
+        )
 
         if title:
             chart = chart.properties(title=title)
 
-        if return_chart:
-            return chart
+        if haxis:
+            if return_chart:
+                return top_label & chart
+            else:
+                (top_label & chart).display()
         else:
-            chart.interactive().display()
+            if return_chart:
+                return chart
+            else:
+                chart.interactive().display()