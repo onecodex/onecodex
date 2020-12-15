9900b2141a4d76d1fc43d0c9918ab018e0ada159
Paweł Konieczny <pawel@onecodex.com>
2020-12-15 11:51:07 +0100

    Add paired end test scenario

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -247,6 +247,7 @@ def test_empty_upload(runner, mocked_creds_path, upload_mocks):
         (["test.fq"], 1),
         # 2 files, 1 sample
         (["test_R1.fq", "test_R2.fq"], 1),
+        (["dir_r1_test/test_R1.fq", "dir_r1_test/test_R2.fq"], 1),
         (["test_1.fq", "test_2.fq"], 1),
         (["test_1_1.fq", "test_1_2.fq"], 1),
         (["test_S1_L001_R1_001.fastq.gz", "test_S1_L001_R2_001.fastq.gz"], 1),
