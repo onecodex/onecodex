58f08edc79173b6eea862480ee036e85a24d0447
Paweł Konieczny <pawel@onecodex.com>
2020-12-17 17:12:09 +0100

    Refactor make the code python2 friendly

--- tests/test_input_helpers.py
+++ tests/test_input_helpers.py
@@ -1,14 +1,20 @@
 import os
 import gzip
 import pytest
-from onecodex.input_helpers import _find_multiline_groups, concatenate_multiline_files, auto_detect_pairs
+from onecodex.input_helpers import (
+    _find_multiline_groups,
+    concatenate_multiline_files,
+    auto_detect_pairs,
+)
 from tests.conftest import FASTQ_SEQUENCE
 
 
 def _get_basenames(elems):
     return [
-        (os.path.basename(elem[0]), os.path.basename(elem[1])) 
-        if isinstance(elem, tuple) else os.path.basename(elem) for elem in elems
+        (os.path.basename(elem[0]), os.path.basename(elem[1]))
+        if isinstance(elem, tuple)
+        else os.path.basename(elem)
+        for elem in elems
     ]
 
 
@@ -17,14 +23,12 @@ def _get_basenames(elems):
     [
         (["test.fq"], ["test.fq"]),
         (["test_R1.fq", "test_R2.fq"], [("test_R1.fq", "test_R2.fq")]),
-        (
-            ["dir_r1_test/test_R1.fq", "dir_r1_test/test_R2.fq"], [("test_R1.fq", "test_R2.fq")]
-        ),
+        (["dir_r1_test/test_R1.fq", "dir_r1_test/test_R2.fq"], [("test_R1.fq", "test_R2.fq")]),
         (["test_1.fq", "test_2.fq"], [("test_1.fq", "test_2.fq")]),
         (["test_1_1.fq", "test_1_2.fq"], [("test_1_1.fq", "test_1_2.fq")]),
         (
-            ["test_S1_L001_R1_001.fastq.gz", "test_S1_L001_R2_001.fastq.gz"], 
-            [("test_S1_L001_R1_001.fastq.gz", "test_S1_L001_R2_001.fastq.gz")]
+            ["test_S1_L001_R1_001.fastq.gz", "test_S1_L001_R2_001.fastq.gz"],
+            [("test_S1_L001_R1_001.fastq.gz", "test_S1_L001_R2_001.fastq.gz")],
         ),
         (["test_R1.fq", "test_R2.fq", "other.fq"], [("test_R1.fq", "test_R2.fq"), "other.fq"]),
     ],
@@ -38,9 +42,9 @@ def test_auto_detect_pairs(generate_fastq, files, expected_pairing):
 
 def test_find_multiline_groups():
     files = [
-        ("Sample1_L001_R1.fq", "Sample1_L001_R2.fq"), 
-        ("Sample1_L002_R1.fq", "Sample1_L002_R2.fq"), 
-        ("Sample1_L003_R1.fq", "Sample1_L003_R2.fq"), 
+        ("Sample1_L001_R1.fq", "Sample1_L001_R2.fq"),
+        ("Sample1_L002_R1.fq", "Sample1_L002_R2.fq"),
+        ("Sample1_L003_R1.fq", "Sample1_L003_R2.fq"),
         ("Sample2_L001_R1.fq", "Sample2_L001_R2.fq"),
         ("Sample2_L003_R1.fq", "Sample2_L003_R2.fq"),  # proper paired group
         ("Sample3_R1.fq", "Sample3_R2.fq"),  # no multiline
@@ -55,10 +59,12 @@ def test_find_multiline_groups():
         "Sample7_L004.fq",  # sequence gap
     ]
     expected_groups = [
-        ['Sample6_L001.fq', 'Sample6_L002.fq', 'Sample6_L003.fq'],
-        [('Sample1_L001_R1.fq', 'Sample1_L001_R2.fq'), 
-        ('Sample1_L002_R1.fq', 'Sample1_L002_R2.fq'), 
-        ('Sample1_L003_R1.fq', 'Sample1_L003_R2.fq')],
+        ["Sample6_L001.fq", "Sample6_L002.fq", "Sample6_L003.fq"],
+        [
+            ("Sample1_L001_R1.fq", "Sample1_L001_R2.fq"),
+            ("Sample1_L002_R1.fq", "Sample1_L002_R2.fq"),
+            ("Sample1_L003_R1.fq", "Sample1_L003_R2.fq"),
+        ],
     ]
 
     groups = _find_multiline_groups(files)
@@ -67,8 +73,8 @@ def test_find_multiline_groups():
 
 def test_concatenate_multiline_files(generate_fastq):
     pairs = [
-        (generate_fastq("Sample1_L001_R1.fq"), generate_fastq("Sample1_L001_R2.fq")), 
-        (generate_fastq("Sample1_L002_R1.fq"), generate_fastq("Sample1_L002_R2.fq"))
+        (generate_fastq("Sample1_L001_R1.fq"), generate_fastq("Sample1_L001_R2.fq")),
+        (generate_fastq("Sample1_L002_R1.fq"), generate_fastq("Sample1_L002_R2.fq")),
     ]
     singles = [
         generate_fastq("Sample2_L001.fq"),
@@ -77,15 +83,15 @@ def test_concatenate_multiline_files(generate_fastq):
     ]
     non_multiline = [("Sample3_R1.fq", "Sample3_R2.fq"), "Sample3.fq"]
     files = [*pairs, *singles, *non_multiline]
-    
+
     concatenated = concatenate_multiline_files(files, prompt=False)
-    
+
     basenames = _get_basenames(concatenated)
     assert basenames == [*non_multiline, "Sample2.fq", ("Sample1_R1.fq", "Sample1_R2.fq")]
 
     with open(concatenated[len(non_multiline)], "r") as inf:
         assert inf.read() == len(singles) * FASTQ_SEQUENCE
-    
+
     with open(concatenated[len(non_multiline) + 1][0], "r") as inf:
         assert inf.read() == len(pairs) * FASTQ_SEQUENCE
 
@@ -99,4 +105,4 @@ def test_concatenate_gzipped_multiline_files(generate_fastq_gz):
     concatenated = concatenate_multiline_files(files, prompt=False)
     assert len(concatenated) == 1
     with gzip.open(concatenated[0], "r") as fin:
-        assert fin.read() == len(files) * FASTQ_SEQUENCE.encode('utf-8')
+        assert fin.read() == len(files) * FASTQ_SEQUENCE.encode("utf-8")
