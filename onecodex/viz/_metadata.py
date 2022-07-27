3aaf794e1c1fb6f2b20b0dbd517263d20c56690b
Gerrit Gerritsen <gerrit.sage.gerritsen@gmail.com>
2022-07-27 11:42:08 -0700

    Set median box plot line stroke to black (#426)
    Co-authored-by: Jai Ram Rideout <jai.rideout@invitae.com>

--- onecodex/viz/_metadata.py
+++ onecodex/viz/_metadata.py
@@ -1,7 +1,5 @@
-import warnings
-
 from onecodex.lib.enums import AlphaDiversityMetric, Rank, BaseEnum
-from onecodex.exceptions import OneCodexException, PlottingException, PlottingWarning
+from onecodex.exceptions import OneCodexException, PlottingException
 from onecodex.viz._primitives import prepare_props, sort_helper, get_base_classification_url
 
 
@@ -197,26 +195,6 @@ class VizMetadataMixin(object):
             if sort_x:
                 raise OneCodexException("Must not specify sort_x when plot_type is boxplot")
 
-            # See the following issue in case this gets fixed in altair:
-            # https://github.com/altair-viz/altair/issues/2144
-            if facet_by:
-                faceted_dfs = [faceted_df for _, faceted_df in df.groupby(facet_by)]
-            else:
-                faceted_dfs = [df]
-
-            warn_on_empty_boxes = False
-            for faceted_df in faceted_dfs:
-                if (faceted_df.groupby(magic_fields[haxis]).size() < 2).any():
-                    warn_on_empty_boxes = True
-                    break
-
-            if warn_on_empty_boxes:
-                warnings.warn(
-                    "There is at least one sample group consisting of only a single sample. Groups "
-                    "of size 1 may not have their boxes displayed in the plot.",
-                    PlottingWarning,
-                )
-
             box_size = 45
             increment = 5
 
@@ -227,7 +205,7 @@ class VizMetadataMixin(object):
 
             chart = (
                 alt.Chart(df)
-                .mark_boxplot(size=box_size)
+                .mark_boxplot(size=box_size, median={"stroke": "black"})
                 .encode(
                     x=alt.X(magic_fields[haxis], axis=alt.Axis(title=xlabel)),
                     y=alt.Y(magic_fields[vaxis], axis=alt.Axis(title=ylabel)),
