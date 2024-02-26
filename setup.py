c54a0ad9ddca931e3af09345cc504d5fc9096be7
Jai Ram Rideout <jai@onecodex.com>
2024-02-26 12:25:35 -0700

    Add beta diversity stats tests
    Added PERMANOVA test for significant differences between groups of samples based on their beta diversity distances.
    
    Posthoc pairwise PERMANOVA tests are performed if the global test is found to be statistically significant and there are more than two groups.
    
    The tests are run via `SampleCollection.beta_diversity_stats()`.
    
    Closes DEV-9248

--- setup.py
+++ setup.py
@@ -38,13 +38,13 @@ TESTING_DEPS = [
 ]
 ALL_DEPS = [
     "altair==4.2.2",  # altair_saver doesn't support altair 5
-    "numpy>=1.11.0",
+    "numpy>=1.21.6",
     "pandas>=1.0.3",
     "pillow>=9.0.1",
     "scikit-bio>=0.5.8",
     "scikit-learn>=0.19.0",
     "scikit-posthocs",
-    "scipy",
+    "scipy>=1.11.0",
     "jupyter-client==8.6.0",
 ]
 REPORT_DEPS = ALL_DEPS + [
