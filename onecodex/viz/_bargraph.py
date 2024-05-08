Fix calculation when all samples lack abundance estimates

onecodex/viz/_bargraph.py
@@ -88,6 +88,7 @@ class VizBargraphMixin(object):
 
         >>> plot_bargraph(rank='genus', top_n=10)
         """
+
         # Deferred imports
         import altair as alt
 
@@ -150,8 +151,8 @@ class VizBargraphMixin(object):
             # Replace nans with zeros for samples that have a total abundance of zero.
             df = df.div(df.sum(axis=1), axis=0).fillna(0.0)
 
-        # Keep track of empty rows *before* filtering taxa by threshold/top_n. We'll use this below
-        # to calculate "Other".
+        # Keep track of empty rows *before* filtering taxa by threshold/top_n.
+        # We'll use this below to calculate "Other".
         empty_rows = df[df.sum(axis=1) == 0.0].index
 
         if top_n == "auto" and threshold == "auto":
@@ -170,7 +171,8 @@ class VizBargraphMixin(object):
 
         if include_other and normalize:
             df["Other"] = df.apply(
-                lambda row: 0.0 if row.name in empty_rows else 1 - row.sum(), axis=1
+                lambda row: 0.0 if (row.name is None or row.name in empty_rows) else 1 - row.sum(),
+                axis=1,
             )
 
         if isinstance(legend, str):