557f756ab3ed690dc74f94abd93b2e0efa81a116
Nick Greenfield <nick.greenfield@gmail.com>
2025-07-11 10:39:08 -0400

    Test updates for new HTTPClient on API

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -8,7 +8,7 @@ from onecodex.lib.auth import BearerTokenAuth, check_version
 def test_bearer_auth_from_env(api_data, monkeypatch):
     monkeypatch.setenv("ONE_CODEX_BEARER_TOKEN", "mysecrettoken")
     ocx = Api(base_url="http://localhost:3000", cache_schema=True)
-    assert isinstance(ocx._req_args["auth"], BearerTokenAuth)
+    assert isinstance(ocx._client.session.auth, BearerTokenAuth)
     sample = ocx.Samples.get("761bc54b97f64980")
     assert sample.visibility != "public"
 
@@ -16,17 +16,17 @@ def test_bearer_auth_from_env(api_data, monkeypatch):
 def test_api_key_auth_from_env(api_data, monkeypatch):
     monkeypatch.setenv("ONE_CODEX_API_KEY", "mysecretkey")
     ocx = Api(base_url="http://localhost:3000", cache_schema=True)
-    assert isinstance(ocx._req_args["auth"], HTTPBasicAuth)
+    assert isinstance(ocx._client.session.auth, HTTPBasicAuth)
 
 
 def test_bearer_auth_from_kwargs(api_data):
     ocx = Api(bearer_token="mysecrettoken", base_url="http://localhost:3000", cache_schema=True)
-    assert isinstance(ocx._req_args["auth"], BearerTokenAuth)
+    assert isinstance(ocx._client.session.auth, BearerTokenAuth)
 
 
 def test_api_key_auth_from_kwargs(api_data):
     ocx = Api(api_key="mysecretkey", base_url="http://localhost:3000", cache_schema=True)
-    assert isinstance(ocx._req_args["auth"], HTTPBasicAuth)
+    assert isinstance(ocx._client.session.auth, HTTPBasicAuth)
 
 
 @pytest.mark.parametrize(
