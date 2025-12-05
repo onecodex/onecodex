4dc3a8b4203f599ee262cfa3b028e76bcbcad87b
Jai Ram Rideout <jai@onecodex.com>
2025-12-05 11:27:47 -0700

    Custom Plots: replace `requests` with `pyfetch` (#566)
    Replaced `requests` with Pyodide's `pyfetch` in Custom Plots.
    
    Closes DEV-11106

--- onecodex/viz/_custom_plots/__init__.py
+++ onecodex/viz/_custom_plots/__init__.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Callable
+from typing import TYPE_CHECKING, Any, Callable, Tuple
+from urllib.parse import urlencode
 import json
 
-from onecodex.utils import get_requests_session
 from onecodex.viz import configure_onecodex_theme
 from onecodex.exceptions import OneCodexException
 from .collection import SampleCollection, Samples
@@ -12,6 +12,7 @@ from .models import PlotParams
 
 if TYPE_CHECKING:
     from pyodide.ffi import JsProxy
+    from pyodide.http import FetchResponse
 
 CUSTOM_PLOTS_CACHE = {}
 
@@ -20,13 +21,11 @@ def init():
     configure_onecodex_theme()
 
 
-def plot(
+async def plot(
     params: JsProxy,
     csrf_token: str,
     progress_callback: Callable[[str, float], None] = lambda msg, pct: None,
 ) -> dict:
-    import js  # available from pyodide
-
     params = PlotParams.model_validate(_convert_jsnull_to_none(params.to_py()))
 
     uuid = None
@@ -45,23 +44,9 @@ def plot(
     if key in CUSTOM_PLOTS_CACHE:
         collection = CUSTOM_PLOTS_CACHE[key]
     else:
-        base_url = js.self.location.origin
-        url = f"{base_url}/api/v2/custom-plots/sample-data"
-        session = get_requests_session(headers={"X-CSRFToken": csrf_token})
-
-        samples = []
-        next_page = 1
-        progress_callback("Loading samples", 0.0)
-        while next_page:
-            resp = session.get(url, params={"type": type_, "uuid": uuid, "page": next_page})
-            resp.raise_for_status()
-            samples.extend(Samples(sample) for sample in resp.json())
-
-            pagination = json.loads(resp.headers.get("X-Pagination", "{}"))
-            total = int(pagination.get("total", 0))
-            next_page = int(pagination.get("next_page", 0))
-            progress_callback("Loading samples", len(samples) / (total or 1))
-
+        samples = await _fetch_samples(
+            type_=type_, uuid=uuid, csrf_token=csrf_token, progress_callback=progress_callback
+        )
         collection = SampleCollection(samples, metric=params.metric)
         CUSTOM_PLOTS_CACHE[key] = collection
 
@@ -89,6 +74,74 @@ def _convert_jsnull_to_none(obj: Any) -> Any:
     elif isinstance(obj, dict):
         return {k: _convert_jsnull_to_none(v) for k, v in obj.items()}
     return obj
+
+
+async def _fetch_samples(
+    *,
+    type_: SuggestionType,
+    uuid: str,
+    csrf_token: str,
+    progress_callback: Callable[[str, float], None] = lambda msg, pct: None,
+) -> list[Samples]:
+    import js  # available from pyodide
+
+    base_url = js.self.location.origin
+    url = f"{base_url}/api/v2/custom-plots/sample-data"
+    headers = {
+        "X-CSRFToken": csrf_token,
+        "Accept": "application/json",
+    }
+
+    samples = []
+    next_page = 1
+    progress_callback("Loading samples", 0.0)
+    while next_page:
+        params = urlencode(
+            {
+                "type": type_,
+                "uuid": uuid,
+                "page": next_page,
+            }
+        )
+        full_url = f"{url}?{params}"
+
+        resp = await _fetch_with_retries(url=full_url, headers=headers)
+        resp.raise_for_status()
+
+        data = await resp.json()
+        samples.extend(Samples(sample) for sample in data)
+
+        pagination = json.loads(resp.headers.get("x-pagination", "{}"))
+        total = int(pagination.get("total", 0))
+        next_page = int(pagination.get("next_page", 0))
+        progress_callback("Loading samples", len(samples) / (total or 1))
+
+    return samples
+
+
+async def _fetch_with_retries(
+    *,
+    url: str,
+    method: str = "GET",
+    headers: dict | None = None,
+    timeout: int = 30,
+    retries: int = 3,
+    backoff_factor: float = 4.0,
+    status_forcelist: Tuple[int, ...] = (429, 502, 503),
+) -> FetchResponse:
+    import asyncio
+    from pyodide.http import pyfetch
+
+    # We're not using `requests` because it doesn't always work reliably in Pyodide, e.g. with
+    # retries or large response payloads. It also generates console errors about setting request
+    # headers that are blocked by the browser.
+    for attempt in range(retries + 1):
+        resp = await asyncio.wait_for(pyfetch(url, method=method, headers=headers), timeout=timeout)
+        if attempt < retries and resp.status in status_forcelist:
+            delay = backoff_factor * (2**attempt)  # exponential backoff
+            await asyncio.sleep(delay)
+        else:
+            return resp
 
 
 __all__ = ["init", "plot"]
