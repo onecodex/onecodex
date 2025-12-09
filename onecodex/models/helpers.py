ad3f71ff864a78dd6cdbe9f821c8acf1486c234c
Petras <petras@onecodex.com>
2025-12-09 16:42:42 +0200

    Removing unused param

--- onecodex/models/helpers.py
+++ onecodex/models/helpers.py
@@ -115,7 +115,6 @@ class ResourceDownloadMixin(object):
         path=None,
         file_obj=None,
         progressbar=False,
-        get_link_callback=None,
     ):
         from requests.adapters import HTTPAdapter
         from requests.packages.urllib3.util.retry import Retry
