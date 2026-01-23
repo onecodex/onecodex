e58337cc3238d0e3435922138a31f35720aa57b7
Alena <alenajk@users.noreply.github.com>
2026-01-23 15:31:27 -0500

    use plottingexception for no taxa for rank error

--- onecodex/analyses.py
+++ onecodex/analyses.py
@@ -5,7 +5,7 @@ from collections import Counter
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any, Optional, Union, Literal
 
-from onecodex.exceptions import OneCodexException
+from onecodex.exceptions import OneCodexException, PlottingException
 from onecodex.lib.enums import (
     AbundanceMetric,
     AnalysisType,
@@ -490,7 +490,9 @@ class AnalysisMixin(
                 tax_ids_to_keep.append(no_level_name)
 
             if len(tax_ids_to_keep) == 0:
-                raise OneCodexException(f"No taxa kept--is rank ({rank.value}) correct?")
+                raise PlottingException(
+                    f"There are no {rank.value}-level results for the selected samples. Please choose a different rank to plot."
+                )
 
             df = df.loc[:, tax_ids_to_keep]
 
