Wrong order of raised

tests/test_viz.py
@@ -1109,7 +1109,7 @@ def test_plot_bargraph_include_taxa_missing_rank(
 
 
 def test_which_fails_in_wasm():
-    a = np.intp((32 - 1) ** 2 - 1)
+    a = np.intp(2 ** (32 - 1) - 1)
     b = np.intp(2)
 
     assert a * b > 0