42f5cb2ccecdbdafa4a0a01002385c63bf76a4a4
Austin Richardson <accounts@agdr.org>
2026-04-21 11:47:30 -0700

    Revert change

--- onecodex/models/base.py
+++ onecodex/models/base.py
@@ -46,6 +46,10 @@ class ApiRef(PydanticBaseModel):
             return {"$ref": data["field_uri"]}
         return data
 
+    def __init__(self, **data):
+        super().__init__(**data)
+        self._resolved_cache = None
+
     @property
     def id(self) -> str:
         """Extract the ID from the reference URI."""
